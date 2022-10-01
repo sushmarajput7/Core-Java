e7721f12d2e148d7ca0326eb77167d10c0c03507
Sushma <114230848+sushmarajput7@users.noreply.github.com>
2022-10-01 13:58:46 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,20 @@
+package corejava;
+
+import java.util.Scanner;
+
+class Unary {
+
+	public static void main(String[] args) {
+		Scanner sc = new scanner (System.in);
+		System.out.println ("Enter the number: ");
+		int num1 = sc.nextInt();
+		//performing Unary operation
+		 num1 =-num1;
+		 System.out.println("Result is :" +num1);
+		 
+		 //Performing pre increment operation
+		
+
+	}
+
+}
